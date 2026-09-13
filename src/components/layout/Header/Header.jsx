e3037a321e8cfe923df9622a9a3f20ea62f266fb
Paulo Silva <paulofernando78@gmail.com>
2026-09-13 16:47:32 -0300

    wip

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -1,13 +1,9 @@
-import { Icon } from "@/components/ui/Icon";
-
 export function Header() {
   return (
     <div className="flex justify-between items-center gap-2 mb-6 p-2 border-b border-gray-700">
       <div className="flex items-center gap-2">
-        <Icon name="galleryHorizontalEnd" />
-        <h1 className="text-lg text-white font-bold uppercase">
-          dashfold
-        </h1>
+        <img src="/favicon.svg"/>
+        <h1 className="text-lg text-white font-bold uppercase">dashfold</h1>
       </div>
       <div className="space-x-2 font-bold">
         <button className="clickable clickable-label">Eng</button>

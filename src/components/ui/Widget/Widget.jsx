15ed7002d01a898bcc1611e86cdb5b7d26231127
Paulo Silva <paulofernando78@gmail.com>
2026-09-14 21:34:52 -0300

    wip

--- src/components/ui/Widget/Widget.jsx
+++ src/components/ui/Widget/Widget.jsx
@@ -3,7 +3,7 @@ import { Icon } from "@/components/ui/Icon";
 export function WidgetContainer({ children }) {
   return <div className="flex gap-2">{children}</div>;
 }
-export const widgetHeight = `h-[]`;
+export const widgetHeight = `h-[468px]`;
 
 // + WidgetPicker
 export const widgetBorder = `

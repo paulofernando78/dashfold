9399a890e52d7feada7c3cddd245326ffe98b515
Paulo Silva <paulofernando78@gmail.com>
2026-09-16 03:23:12 -0300

    wip

--- src/components/features/Timer/Timer.jsx
+++ src/components/features/Timer/Timer.jsx
@@ -211,7 +211,7 @@ export function Timer({
             <span
               className={`
               justify-self-center
-              text-2xl
+              text-3xl
               font-bold
               uppercase
               pt-6
